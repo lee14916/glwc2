0d7fe6cdc0369ff50a55880231a4e99b0a88b229
Yan Li <yan1491625@gmail.com>
2025-08-19 16:35:21 +0300

    sync

--- project2/02_discNJN_1D/dataPrepare/cB211.072.64/scp.sh
+++ project2/02_discNJN_1D/dataPrepare/cB211.072.64/scp.sh
@@ -1,2 +1,4 @@
-# scp -r /nvme/h/cy22yl1/projectData/02_discNJN_1D/cB211.072.64/data_post/ li47@juwels-booster.fz-juelich.de:/p/project/ngff/li47/code/projectData/02_discNJN_1D/cB211.072.64/loop_cyclone/
-rsync -av --progress /nvme/h/cy22yl1/projectData/02_discNJN_1D/cB211.072.64/data_post/ li47@juwels-booster.fz-juelich.de:/p/project/ngff/li47/code/projectData/02_discNJN_1D/cB211.072.64/loop_cyclone/
+ens=cB211.072.64
+
+# scp -r /nvme/h/cy22yl1/projectData/02_discNJN_1D/${ens}/data_post/ li47@juwels-booster.fz-juelich.de:/p/project/ngff/li47/code/projectData/02_discNJN_1D/${ens}/loop_cyclone/
+rsync -av --progress /nvme/h/cy22yl1/projectData/02_discNJN_1D/${ens}/data_post/ li47@login.jupiter.fz-juelich.de:/p/project1/ngff/li47/code/projectData/02_discNJN_1D/${ens}/cyclone/
